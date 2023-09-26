d91c1f1b9f353f258a3a52e60f7b12f34bdd2a86
BB-Code <15919854639@163.com>
2023-09-26 10:42:02 +0800

    fix: udpate prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,6 @@ module.exports = {
     trailingComma: 'none',
     singleQuote: true,
     tabWidth: 4,
+    extends: ['eslint:recommended','prettier'],
+    plugins: ['@typescript-eslint','prettier'],
 }
